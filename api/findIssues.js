22181f8902232ccec5ca8c534c5a62b4913b43ae
Glen Keane <glenkeane.94@gmail.com>
2020-01-31 18:15:20 +0000

    extend api

--- api/findIssues.js
+++ api/findIssues.js
@@ -15,27 +15,37 @@ const handler = async (req, res) => {
   const includeBody = params.get('includeBody') || false
 
   const itemSearchResults = (await Promise.all(labels.map(async label => {
-	const issues = await octokit.search.issuesAndPullRequests({
-		q: `is:issue is:open sort:updated-desc label:"${label}" org:"${org}"`
-	})
+	  const issues = await octokit.search.issuesAndPullRequests({
+	  	q: `is:issue is:open sort:updated-desc label:"${label}" org:"${org}"`
+	  })
 
-	return issues.data.items
+	  return issues.data.items
   })))
   const items = itemSearchResults.flat()
-
+	console.log(items)
 	const dedupeMap = new Map()
   for (let item of items) {
-	dedupeMap.set(item.html_url, {
-    url: item.html_url,
-		title: item.title,
-		comments: item.comments,
-	  body: includeBody ? item.body : undefined,
-  	  author: {
-        name: item.user.login,
-        avatar_url: item.user.avatar_url,
-        acc_url: item.user.html_url
-	  },
-	  labels: item.labels.map(({name}) => name)
+		const [_, orgMatch, repoNameMatch] = item.repository_url.match(/https:\/\/api\.github\.com\/repos\/([a-zA-Z0-9\-_]+)\/([a-zA-Z0-9\-_]+)/)
+	  dedupeMap.set(item.html_url, {
+      url: item.html_url,
+	  	title: item.title,
+	  	comments: item.comments,
+	    body: includeBody ? item.body : undefined,
+    	author: {
+          name: item.user.login,
+          avatar_url: item.user.avatar_url,
+          acc_url: item.user.html_url
+	  	},
+	  	project: {
+				org: orgMatch,
+	  		name: repoNameMatch,
+	  		url: `https://github.com/${orgMatch}/${repoNameMatch}`
+			},
+			locked: item.locked,
+			state: item.state,
+			created_at: item.created_at,
+			updated_at: item.updated_at,
+	    labels: item.labels.map(({name}) => name)
     })
   }
 
